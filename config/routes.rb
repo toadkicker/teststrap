b4c83fb071bb40c65e58dd10d195dc3190fa07d4
Todd Baur <todd@toadkicker.com>
2012-10-31 11:28:19 -0700

    update ui

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Teststrap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'people#index'
 
   # See how all your routes lay out with "rake routes"
 
