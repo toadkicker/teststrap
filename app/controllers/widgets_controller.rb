f292f58d25cd9ce9b270a0eb4edb49fe4eb44641
Todd Baur <todd@toadkicker.com>
2014-10-03 22:53:47 +0300

    using railsstrap gem

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -5,7 +5,7 @@ class WidgetsController < ApplicationController
   # GET /widgets
   def index
     @widgets = Widget.all
-    flash[:success] = 'Welcome to Teststrap. An example Rails 4.1.6 application using twitter-bootstrap-rails'
+    flash[:success] = 'Welcome to Teststrap. An example Rails 4.1.6 application using railsstrap'
   end
 
   # GET /widgets/1
