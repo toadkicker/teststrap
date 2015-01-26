5481d999680bd4d3d027435ce909e45a5a484f52
Todd Baur <todd@toadkicker.com>
2015-01-25 23:11:56 -0500

    added error handling for 404 and obj not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  rescue_from ActionController::UnknownController, with: -> { render_404  }
+
+  def record_not_found
+    flash[:notice] = 'The object you tried to access does not exist'
+    redirect_to :action => :index
+  end
+
+  unless Rails.application.config.consider_all_requests_local # so that in development you would see real exceptions, but not in production.
+    rescue_from ActionController::RoutingError, with: -> { render_404  }
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :widgets
+  unless Rails.application.config.consider_all_requests_local
+    get '*not_found', to: 'application#render_404'
+  end
   root to: "widgets#index"
+
 end
